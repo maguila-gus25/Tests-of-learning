c5cc899e20c7995e5d24a2608dd32dfcfb88ff09
maguila-gus25 <166073286+maguila-gus25@users.noreply.github.com>
2024-09-09 18:59:59 -0300

    Update pessoa.py

--- pessoa.py
+++ pessoa.py
@@ -1,11 +1,22 @@
 class Pessoa:
-  def __init__(self, nome=""):
-    self.__nome = nome
-  
-  @property
-  def nome(self):
-      return self.__nome
-  
-  @nome.setter
-  def nome(self, nome):
-    self.__nome = nome
+    def __init__(self, nome=""):
+        self.__nome = nome
+
+    def __init__(self, idade=int):
+        self.__idade = idade
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @idade.setter
+    def idade(self, idade):
+        self.__idade = idade
